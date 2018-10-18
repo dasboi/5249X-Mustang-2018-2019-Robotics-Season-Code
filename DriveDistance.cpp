267cad997d345285afaad330a00b0e5a8d7cc5a6
Some Dude Bro <dennis.lego@aceweb.com>
2018-10-18 12:50:04 -0400

    got rid of them pesky comments

--- DriveDistance.cpp
+++ DriveDistance.cpp
@@ -2,23 +2,19 @@
 #include "math.h" //Include math.h in order to gain access to math functions like PI. 
 
 int main() {
-     vex::task::sleep(2000); //Wait 2 seconds or 2000 milliseconds before starting the program.
-     //Print to the screen that the program has started.
+     vex::task::sleep(2000); 
      Brain.Screen.print("User Program has Started.");
     
-    //Define variables. You may need to change these variables for this program to work for your robot.
-    double wheelDiameterCM  = 10.16; //wheelDiameter is the measurement of a wheel from edge to edge in centimeters.
-    double travelTargetCM = 90; //travelTarget will define how far we want the robot to move in centimeters.
     
-    //Calculate wheel circumferance: circumference = (wheelDiameter)(PI)
-    double circumference = wheelDiameterCM * M_PI; //Note that M_PI is from the math library which is included on line 14.    
-    //Now we know the robot will travel the Circumference per 360 degrees of rotation or (circumference / 360 degrees).
-    //We also know that are target distance is defined as travelTargetCM, but we do not know the degrees to of rotation. (traveTargetCM / ?)
-    //Using propotional reasoning we know (circumference / 360) = (travelTarget / degreesToRotate).
-    //Solving for our unkown (degreesToRotate) we get:
+    double wheelDiameterCM  = 10.16; 
+    double travelTargetCM = 90; 
+    
+   
+    double circumference = wheelDiameterCM * M_PI;
+    
     double degreesToRotate = (360 * travelTargetCM) / circumference; //All calculations are complete. Start the rest of the program.
     
-    //Set the velocity of the left and right motor to 50% power. This command will not make the motor spin.
+    
     FrontLeftMotor.setVelocity(50, vex::velocityUnits::pct); 
     FrontRightMotor.setVelocity(50, vex::velocityUnits::pct);
     BackLeftMotor.setVelocity(50, vex::velocityUnits::pct); 
