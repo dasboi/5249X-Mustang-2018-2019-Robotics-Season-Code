7aad64db4553faf010f192b803a2a749d0b3d338
Some Dude Bro <dennis.lego@aceweb.com>
2018-10-23 16:27:27 -0400

    Update CompetitionCode.cpp

--- CompetitionCode.cpp
+++ CompetitionCode.cpp
@@ -1,61 +1,122 @@
-
 #include "robot-config.h"
+
 /*---------------------------------------------------------------------------*/
+
 /*                                                                           */
+
 /*        Description: Competition template for VCS VEX V5                    */
+
 /*                                                                           */
+
 /*---------------------------------------------------------------------------*/
+
+
 
 //Creates a competition object that allows access to Competition methods.
+
 vex::competition    Competition;
 
+
+
 /*---------------------------------------------------------------------------*/
+
 /*                          Pre-Autonomous Functions                         */
+
 /*                                                                           */
+
 /*  You may want to perform some actions before the competition starts.      */
+
 /*  Do them in the following function.  You must return from this function   */
+
 /*  or the autonomous and usercontrol tasks will not be started.  This       */
+
 /*  function is only called once after the cortex has been powered on and    */ 
+
 /*  not every time that the robot is disabled.                               */
+
 /*---------------------------------------------------------------------------*/
+
+
 
 void pre_auton( void ) {
+
   // All activities that occur before the competition starts
+
   // Example: clearing encoders, setting servo positions, ...
+
   
+
 }
 
+
+
 /*---------------------------------------------------------------------------*/
+
 /*                                                                           */
+
 /*                              Autonomous Task                              */
+
 /*                                                                           */
+
 /*  This task is used to control your robot during the autonomous phase of   */
+
 /*  a VEX Competition.                                                       */
+
 /*                                                                           */
+
 /*  You must modify the code to add your own robot specific commands here.   */
+
 /*---------------------------------------------------------------------------*/
+
+
 
 void autonomous( void ) {
+
   // ..........................................................................
+
   // Insert autonomous user code here.
+
   // ..........................................................................
+
+
 
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
+
+
 /*---------------------------------------------------------------------------*/
 
+/*                                                                           */
+
+/*                              User Control Task                            */
+
+/*                                                                           */
+
+/*  This task is used to control your robot during the user control phase of */
+
+/*  a VEX Competition.                                                       */
+
+/*                                                                           */
+
+/*  You must modify the code to add your own robot specific commands here.   */
+
+/*---------------------------------------------------------------------------*/
+void ControllerLift(bool up){
+    LiftLeft.spin(vex::directionType::fwd);
+    LiftRight.spin(vex::directionType::fwd);
+
+}
+
+
 void usercontrol( void ) {
+
   // User control code here, inside the loop
   while (1) {
       
+    
+    //Controller.ButtonR2.pressing(ControllerLift(true)) ERROR
+     
+    
     /*
     what it should be
     motor[leftfront] = VexRT[ch3] + VexRT[ch1] + VexRT[ch4];
@@ -64,41 +125,85 @@ void usercontrol( void ) {
     Motor[rightrear]= VexRT[ch3] - VexRT[ch1] + VexRT[ch4];
     */
     
-    FrontLeftMotor.spin(vex::directionType::fwd, Controller.Axis3.value(), vex::velocityUnits::pct);
-    FrontRightMotor.spin(vex::directionType::fwd, Controller.Axis3.value(), vex::velocityUnits::pct);
-    BackLeftMotor.spin(vex::directionType::fwd, Controller.Axis3.value(), vex::velocityUnits::pct);
-    BackRightMotor.spin(vex::directionType::fwd, Controller.Axis3.value(), vex::velocityUnits::pct);
+
       
+
+    FrontLeftMotor.spin(vex::directionType::fwd, Controller.Axis3.value() + Controller.Axis1.value() + Controller.Axis4.value(), vex::velocityUnits::pct);
+    FrontRightMotor.spin(vex::directionType::fwd, Controller.Axis3.value() - Controller.Axis1.value() - Controller.Axis4.value(), vex::velocityUnits::pct);
+    BackLeftMotor.spin(vex::directionType::fwd, Controller.Axis3.value() + Controller.Axis1.value() - Controller.Axis4.value(), vex::velocityUnits::pct);
+    BackRightMotor.spin(vex::directionType::fwd, Controller.Axis3.value() - Controller.Axis1.value() + Controller.Axis4.value(), vex::velocityUnits::pct);
+    
+    
+      /*if (Controller.ButtonR2 == true){
+          LiftLeft.power(vex::powerUnits::
+      }
+      */
+   
+
       
-     //Controller.rumble(".-.-");
+
+      
+
+     //Controller.rumble(".-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+
     // This is the main execution loop for the user control program.
+
     // Each time through the loop your program should update motor + servo 
+
     // values based on feedback from the joysticks.
 
+
+
     // ........................................................................
+
     // Insert user code here. This is where you use the joystick values to 
+
           // update your motors, etc.
+
     // ........................................................................
+
  
+
     vex::task::sleep(20); //Sleep the task for a short amount of time to prevent wasted resources. 
+
   }
+
 }
 
+
+
 //
+
 //Controller.Screen.clearLine(1);// Main will set up the competition functions and callbacks.
+
 //
+
 int main() {
+
     
+
     //Run the pre-autonomous function. 
+
     pre_auton();
+
     
+
     //Set up callbacks for autonomous and driver control periods.
+
     Competition.autonomous( autonomous );
+    
     Competition.drivercontrol( usercontrol );
 
+
+
     //Prevent main from exiting with an infinite loop.                        
+
     while(1) {
+
       vex::task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
+
     }    
+
        
+
 }
