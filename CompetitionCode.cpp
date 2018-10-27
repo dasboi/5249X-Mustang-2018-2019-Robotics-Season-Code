e555a1fc133bdc99d29b921808ef6289f4a99c6c
Dennis The Dude <dennis.lego@aceweb.com>
2018-10-27 09:14:37 -0700

    Update CompetitionCode.cpp

--- CompetitionCode.cpp
+++ CompetitionCode.cpp
@@ -1,260 +1,70 @@
 #include "robot-config.h"
-
-
-
 /*---------------------------------------------------------------------------*/
-
-
-
 /*                                                                           */
-
-
-
 /*        Description: Competition template for VCS VEX V5                    */
-
-
-
 /*                                                                           */
-
-
-
 /*---------------------------------------------------------------------------*/
-
-
-
-
-
-
 
 //Creates a competition object that allows access to Competition methods.
 
-
-
 vex::competition    Competition;
-
-
-
-
-
-
-
 /*---------------------------------------------------------------------------*/
-
-
-
 /*                          Pre-Autonomous Functions                         */
-
-
-
 /*                                                                           */
-
-
-
 /*  You may want to perform some actions before the competition starts.      */
-
-
-
 /*  Do them in the following function.  You must return from this function   */
-
-
-
 /*  or the autonomous and usercontrol tasks will not be started.  This       */
-
-
-
 /*  function is only called once after the cortex has been powered on and    */ 
-
-
-
 /*  not every time that the robot is disabled.                               */
-
-
-
 /*---------------------------------------------------------------------------*/
-
-
-
-
-
-
-
 void pre_auton( void ) {
-
-
-
   // All activities that occur before the competition starts
-
-
-
   // Example: clearing encoders, setting servo positions, ...
-
-
-
-  
-
-
-
 }
-
-
-
-
-
-
-
 /*---------------------------------------------------------------------------*/
-
-
-
 /*                                                                           */
-
-
-
 /*                              Autonomous Task                              */
-
-
-
 /*                                                                           */
-
-
-
 /*  This task is used to control your robot during the autonomous phase of   */
-
-
-
 /*  a VEX Competition.                                                       */
-
-
-
 /*                                                                           */
-
-
-
 /*  You must modify the code to add your own robot specific commands here.   */
-
-
-
 /*---------------------------------------------------------------------------*/
-
-
-
-
-
-
-
 void autonomous( void ) {
-
-
-
   // ..........................................................................
-
-
 
   // Insert autonomous user code here.
-
-
-
   // ..........................................................................
-
-
-
-
-
-
-
 }
 
-
-
-
-
-
-
 /*---------------------------------------------------------------------------*/
-
-
-
 /*                                                                           */
-
-
-
 /*                              User Control Task                            */
-
-
-
 /*                                                                           */
-
-
-
 /*  This task is used to control your robot during the user control phase of */
-
-
-
 /*  a VEX Competition.                                                       */
-
-
-
 /*                                                                           */
-
-
-
 /*  You must modify the code to add your own robot specific commands here.   */
-
-
-
 /*---------------------------------------------------------------------------*/
-
-
 
 //a method for controlling the lift that takes in a boolean value to determine the direction the lift will go 
-
 void ControllerLift(bool up){
-
   if (up){
-
     LiftLeft.spin(vex::directionType::fwd);
-
     LiftRight.spin(vex::directionType::fwd);
-
   }else{
-
     //make sure the syntax is right, not sure what the name for reverse is
-
-    //                                 |
-
+                                
     LiftLeft.spin(vex::directionType::rev);
-
-    //                                 |
-
     LiftRight.spin(vex::directionType::rev);
 
-
-
   }
-
 }
-
-
-
-/*void ControllerFlyWheel(int speed){
-
- if (speed == 1)
-
-   
-
-}
-*/
-
 
 void usercontrol( void ) {
 
-
-
   // User control code here, inside the loop
-
   while (true) {
-
-      
 
     //this if statement is checking the lower right bumper is being pressed, if so, 
 
