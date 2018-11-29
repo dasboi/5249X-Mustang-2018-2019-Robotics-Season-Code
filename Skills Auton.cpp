11151f0b7e6fa53ee1618bef62812e43f6f9e7c4
Dennis The Dude <dennis.lego@aceweb.com>
2018-11-29 14:07:25 -0500

    Update Skills Auton.cpp

--- Skills Auton.cpp
+++ Skills Auton.cpp
@@ -19,12 +19,40 @@ void DriveDistance(int distance, int speed){
 
 //edit this so that it can turn
 void Turn(int degrees, int speed, bool rightTurn){
+    
+    //this is for the speed in the other direction
+    int speedInOtherDirection = speed * -1;
+    
     if (rightTurn == true){
     while(ChasisGyro.value(vex::analogUnits::range8bit) < degrees){
+        
+        FrontLeftMotor.setVelocity(speed, vex::velocityUnits::pct); 
+        FrontRightMotor.setVelocity(speed, vex::velocityUnits::pct);
+        BackLeftMotor.setVelocity(speed, vex::velocityUnits::pct); 
+        BackRightMotor.setVelocity(speed, vex::velocityUnits::pct);
+        
+        
+        //needs to be set to speed
+        FrontLeftMotor.rotateFor(DistanceTheMotorHasToGo, vex::rotationUnits::deg, false);
+        FrontRightMotor.rotateFor(DistanceTheMotorHasToGo, vex::rotationUnits::deg, false);
+        BackLeftMotor.rotateFor(DistanceTheMotorHasToGo, vex::rotationUnits::deg, false);
+        BackRightMotor.rotateFor(DistanceTheMotorHasToGo, vex::rotationUnits::deg, false);
         
     }
    }else{
     while(ChasisGyro.value(vex::analogUnits::range8bit) > degrees){
+         
+        FrontLeftMotor.setVelocity(speed, vex::velocityUnits::pct); 
+        FrontRightMotor.setVelocity(speed, vex::velocityUnits::pct);
+        BackLeftMotor.setVelocity(speed, vex::velocityUnits::pct); 
+        BackRightMotor.setVelocity(speed, vex::velocityUnits::pct);
+        
+        
+        //needs to be set to speed
+        FrontLeftMotor.rotateFor(DistanceTheMotorHasToGo, vex::rotationUnits::deg, false);
+        FrontRightMotor.rotateFor(DistanceTheMotorHasToGo, vex::rotationUnits::deg, false);
+        BackLeftMotor.rotateFor(DistanceTheMotorHasToGo, vex::rotationUnits::deg, false);
+        BackRightMotor.rotateFor(DistanceTheMotorHasToGo, vex::rotationUnits::deg, false);
         
     }    
    }
